f41684ddc4c4a0df8da0bf94c5d9807d38223ccb
Henry Chung <chungthehao@gmail.com>
2019-11-14 23:27:54 +0700

    Routing In Our Project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'; // Switch component đảm bảo tìm được 1 Route match là ko tìm nữa
 
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 
+const HatsPage = () => <h1>Hats Page</h1>;
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
