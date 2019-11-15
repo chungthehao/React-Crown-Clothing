7debda349af31dd74518e2db146ba91b07c4e1d2
Henry Chung <chungthehao@gmail.com>
2019-11-15 07:45:37 +0700

    withRouter()

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'; // higher order component: component -> modified component (có thể access history, location, match ở props)
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={size + ' menu-item'}>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div
+    className={size + ' menu-item'}
+    onClick={() => history.push(match.url + linkUrl)}
+  >
     <div
       className='background-image'
       style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
