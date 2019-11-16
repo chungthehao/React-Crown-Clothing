87df8f8a48b9f46b8ad14fdb8e01a3bc62d856b3
Henry Chung <chungthehao@gmail.com>
2019-11-16 17:29:30 +0700

    Google Sign In Authentication 2

--- src/App.js
+++ src/App.js
@@ -5,20 +5,45 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-&-sign-up/sign-in-&-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    };
+  }
 
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/sign-in' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+  unsubcribeFromAuth = null;
+
+  componentDidMount() {
+    // - Open subscription (an open messaging system) between our app & our firebase at whenever any changes occur on firebase from any source related to this app
+    // We don't actually have to manually fetch every time we want to check if that status changed.
+    this.unsubcribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubcribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/sign-in' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
