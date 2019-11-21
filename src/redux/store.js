8d9fb0d092d04ed0eb7b8993bd443555be77faf4
Henry Chung <chungthehao@gmail.com>
2019-11-21 22:42:31 +0700

    Setup redux dev tool

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger'; // dễ debug redux code
 
 import rootReducer from './root-reducer';
@@ -6,6 +6,10 @@ import rootReducer from './root-reducer';
 // https://redux.js.org/recipes/configuring-your-store#the-solution-configurestore
 const middlewares = [logger]; // Để trg array để ko cần biết là mình xài bao nhiều middlewares thì code ở dưới cũng ko đổi, spread array ra thôi
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export default store;
