7175395949273b72268f1133eaa7816f5d4146ca
Henry Chung <chungthehao@gmail.com>
2019-11-24 00:17:28 +0700

    Selectors in Redux

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -8,12 +8,26 @@ import './cart-icon.styles.scss';
 const CartIcon = props => (
   <div className='cart-icon' onClick={props.toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{props.itemCount}</span>
   </div>
 );
+
+const mapStateToProps = state => {
+  // sign-in, sign-out, toggle cart dropdown cũng gọi nữa?! --> Vì switch case return a brand new state obj
+  // Mặc cho CartIcon này chỉ qtâm cartItems thôi
+  // This is not good for performance -> we don't wanna re-render our component every time the state changes especially if those state changes don't actually modify the parts of the state that our component cares about
+  console.log('I am being called');
+  return {
+    itemCount: state.cart.cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+  };
+};
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
