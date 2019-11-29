321b3fffa068fd6dee6d4fced713b939db1e9d39
Henry Chung <chungthehao@gmail.com>
2019-11-29 22:56:17 +0700

    Remove Items From Cart

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, quantity, price } }) => (
-  <div className='checkout-item'>
-    <div className='image-container'>
-      <img src={imageUrl} alt='item' />
+const CheckoutItem = ({ cartItem, dispatch }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
+  return (
+    <div className='checkout-item'>
+      <div className='image-container'>
+        <img src={imageUrl} alt='item' />
+      </div>
+
+      <span className='name'>{name}</span>
+
+      <span className='quantity'>{quantity}</span>
+
+      <span className='price'>${price}</span>
+
+      <div
+        className='remove-button'
+        onClick={() => dispatch(clearItemFromCart(cartItem))}
+      >
+        &#10005;
+      </div>
     </div>
+  );
+};
 
-    <span className='name'>{name}</span>
-
-    <span className='quantity'>{quantity}</span>
-
-    <span className='price'>${price}</span>
-
-    <div className='remove-button'>&#10005;</div>
-  </div>
-);
-
-export default CheckoutItem;
+export default connect()(CheckoutItem);
