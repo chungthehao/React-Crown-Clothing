e510547fbd08baeb29ad049f54d6cbc0585c507a
Henry Chung <chungthehao@gmail.com>
2019-11-30 22:08:10 +0700

    Collection Routing and Selector

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectShopCollection } from '../../redux/shop/shop.selector';
 import './collection.styles.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log(match);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
@@ -12,4 +14,11 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+// ownProps gives us all of the props that we're getting on our CollectionPage component (bao gồm match obj mà mình có đc trực tiếp từ Route component)
+const mapStateToProps = (state, ownProps) => ({
+  // - selectShopCollection(ownProps.match.params.collectionId) trả về createSelector call
+  // - Khi createSelector đc gọi trả về 1 function nhận tham số là state rồi đi tiếp selector flow như thường lệ (truyền state vô để wire everything together)
+  collection: selectShopCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
