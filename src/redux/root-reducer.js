035a7c57d8ffa033eba7f79b9041e2025f9ca992
Henry Chung <chungthehao@gmail.com>
2019-11-30 14:20:38 +0700

    Directory State Into Redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,17 +4,19 @@ import storage from 'redux-persist/lib/storage'; // import in the type of storag
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 // Định nghĩa persist config (json obj mô tả cái config mình muốn)
 const persistConfig = {
   key: 'root', // Nghĩa là: at what point inside of our reducer object do we want to start storing everything and we want to start from the root
   storage,
-  whitelist: ['cart'] // whitelist là 1 mảng chứa các tên của reducer mà mình muốn store. Ko cần user vì user được xử lý bởi firebase authentication rồi (Có observer lắng nghe r)
+  whitelist: ['cart', 'directory'] // whitelist là 1 mảng chứa các tên của reducer mà mình muốn store. Ko cần user vì user được xử lý bởi firebase authentication rồi (Có observer lắng nghe r)
 };
 
 const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  directory: directoryReducer
 });
 
 // Trả về modified version of our rootReducer with this persistConfig on top of it
