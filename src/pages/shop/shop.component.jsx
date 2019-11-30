00bf5460101e5baa1136b6c840a6e9b6ea2d9e11
Henry Chung <chungthehao@gmail.com>
2019-11-30 21:19:28 +0700

    Improving Naming Of Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CategoryPage from '../category/category.component';
+import CollectionPage from '../collection/collection.component';
 
 // Ở App.js: <Route path='/shop' component={ShopPage} />
 const ShopPage = ({ match }) => {
@@ -10,7 +10,7 @@ const ShopPage = ({ match }) => {
   return (
     <div className='shop-page'>
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
-      <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
