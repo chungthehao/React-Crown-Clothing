63304157d6db71bb16fdfb222c9ce416264c2877
Henry Chung <chungthehao@gmail.com>
2019-12-11 00:21:03 +0700

    WithSpinner HOC 2

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,42 +2,62 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import {connect} from "react-redux";
 
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component';
 import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
 import {updateCollections} from "../../redux/shop/shop.actions";
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component';
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview); // Chinh la Spinner; // trả về 1 component
+const CollectionPageWithSpinner = WithSpinner(CollectionPage); // Chinh la Spinner; // trả về 1 component
 
 class ShopPage extends React.Component {
-    unsubscribeFromSnapshot = null;
+  // Short hand to have state
+  // React tự chạy constructor & super này nọ (các phiên bản React sau này mới có)
+  state = {
+    loading: true
+  };
 
-    componentDidMount() {
-        const { updateCollections } = this.props;
+  unsubscribeFromSnapshot = null;
 
-        const collectionRef = firestore.collection('collections');
+  componentDidMount() {
+    const { updateCollections } = this.props;
 
-        // onSnapshot: whenever the 'collectionRef' updates or whenever this code gets run for the 1st time
-        // this 'collectionRef' will send us the snapshot representing the code of our collections objects array at the time when this code renders.
-        collectionRef.onSnapshot(async snapshot => {
-            // console.log(snapshot); // Là nguyên 1 collection snapshot có tên là 'collections' từ Firebase
+    const collectionRef = firestore.collection('collections');
 
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            // console.log(collectionsMap);
+    // onSnapshot: whenever the 'collectionRef' updates or whenever this code gets run for the 1st time
+    // this 'collectionRef' will send us the snapshot representing the code of our collections objects array at the time when this code renders.
+    collectionRef.onSnapshot(async snapshot => {
+      // console.log(snapshot); // Là nguyên 1 collection snapshot có tên là 'collections' từ Firebase
 
-            // Cập nhật vô redux -> shop -> collections
-            updateCollections(collectionsMap);
-        });
-    }
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      // console.log(collectionsMap);
 
-    render() {
-        const { match } = this.props;
+      // Cập nhật vô redux -> shop -> collections
+      updateCollections(collectionsMap);
 
-        return (
-            <div className='shop-page'>
-                <Route exact path={`${match.path}`} component={CollectionsOverview} />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-            </div>
-        );
-    }
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <div className='shop-page'>
+        {/*<Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />*/}
+
+
+        <Route exact path={`${match.path}`}
+               render={props => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
+        <Route path={`${match.path}/:collectionId`}
+               render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
+
+      </div>
+    );
+  }
 
 }
 
@@ -53,7 +73,7 @@ class ShopPage extends React.Component {
 // };
 
 const mapDispatchToProps = dispatch => ({
-    updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap)),
+  updateCollections: collectionsMap => dispatch(updateCollections(collectionsMap)),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
