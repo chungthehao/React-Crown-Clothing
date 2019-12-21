a5644dda2d721e80bdee09f7b5666c98cf8a2175
Henry Chung <chungthehao@gmail.com>
2019-12-22 02:00:56 +0700

    Clear Cart Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import ShopActionTypes from "./shop.types";
 import {convertCollectionsSnapshotToMap, firestore} from "../../firebase/firebase.utils";
@@ -43,5 +43,10 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCHING_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
 
+export function* shopSagas() {
+  yield all([
+    call(fetchCollectionsStart),
+  ]);
 }
