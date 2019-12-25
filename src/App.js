7230fac929d4898abc8b7aac53bd3be449f4fc79
Henry Chung <chungthehao@gmail.com>
2019-12-25 23:12:07 +0700

    useEffect In Our App (App.js, shop.component.jsx)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom'; // Switch component đảm bảo tìm được 1 Route match là ko tìm nữa
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -10,48 +10,40 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-&-sign-up/sign-in-&-sign-up.component';
 import { selectCurrentUser } from './redux/user/user.selectors';
 import './App.css';
-import {checkUserSession} from "./redux/user/user.actions";
+import { checkUserSession } from './redux/user/user.actions';
 
-class App extends React.Component {
-  componentDidMount() {
-    const { checkUserSession } = this.props;
-
-    // App mình mới chạy thì check liền coi thử coi user session có đâu đó chưa?
+const App = ({ currentUser, checkUserSession }) => {
+  // App mình mới chạy thì check liền coi thử coi user session có đâu đó chưa?
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  render() {
-    return (
-      <div>
-        <Header />
+  return (
+    <div>
+      <Header />
 
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route exact path='/checkout' component={CheckoutPage} />
-          <Route
-            exact
-            path='/sign-in'
-            render={() =>
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>
-      </div>
-    );
-  }
-}
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/checkout' component={CheckoutPage} />
+        <Route
+          exact
+          path='/sign-in'
+          render={() =>
+            currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
+          }
+        />
+      </Switch>
+    </div>
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  checkUserSession: () => dispatch(checkUserSession()),
+  checkUserSession: () => dispatch(checkUserSession())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,62 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
-import {connect} from "react-redux";
+import { connect } from 'react-redux';
 
-import {fetchCollectionsStart} from "../../redux/shop/shop.actions";
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container.jsx';
 import CollectionPageContainer from '../collection/collection.container.jsx';
 
-
-class ShopPage extends React.Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
 
-    // # Nested too much!
-    // fetch('https://firestore.googleapis.com/v1/projects/crwn-db-7c56e/databases/(default)/documents/collections')
-    //   .then(res => res.json())
-    //   .then(myData => console.log(myData.documents))
-
-    // # Chỉ chạy 1 lần rồi thôi!
-    // collectionRef.get().then(snapshot => {
-    //   const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-    //
-    //   // Cập nhật vô redux -> shop -> collections
-    //   updateCollections(collectionsMap);
-    //
-    //   this.setState({ loading: false });
-    // });
-
-    // # Subscribe
-    // onSnapshot: whenever the 'collectionRef' updates or whenever this code gets run for the 1st time
-    // this 'collectionRef' will send us the snapshot representing the code of our collections objects array at the time when this code renders.
-    /*collectionRef.onSnapshot(async snapshot => {
-      // console.log(snapshot); // Là nguyên 1 collection snapshot có tên là 'collections' từ Firebase
-
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      // console.log(collectionsMap);
-
-      // Cập nhật vô redux -> shop -> collections
-      updateCollections(collectionsMap);
-
-      this.setState({ loading: false });
-    });*/
-  }
-
-  render() {
-    const { match } = this.props;
-
-    return (
-      <div className='shop-page'>
-        <Route exact path={`${match.path}`}
-               component={CollectionsOverviewContainer} />
-        <Route path={`${match.path}/:collectionId`}
-               component={CollectionPageContainer} />
-      </div>
-    );
-  }
-
-}
+  return (
+    <div className='shop-page'>
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionsOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
 // Ở App.js: <Route path='/shop' component={ShopPage} />
 // const ShopPage = ({ match }) => {
@@ -69,9 +37,8 @@ class ShopPage extends React.Component {
 //   );
 // };
 
-
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
