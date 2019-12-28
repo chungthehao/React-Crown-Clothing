759ae0970a3b293517388a6bba058b9c39b428f7
Henry Chung <chungthehao@gmail.com>
2019-12-28 07:03:22 +0700

    Backend Payment Route

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const cors = require('cors');
 const bodyParser = require('body-parser'); // Middleware này nhận request và parse dùm mình trước
 const path = require('path'); // native module
 
+// Đưa các thông tin từ file .env vào process.env.XXX
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
+
+// Phải bên dưới dòng env
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000; // when u deploy to Heroku, it sets up the process PORT for you
@@ -25,6 +29,24 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+
+// Xử lý request thanh toán từ client (phía frontend react app)
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  // stripe là obj đã đính stripe secret key để gọi api thanh toán tới stripe server, rồi kết quả thế nào báo lại cho phía client biết.
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
 
 app.listen(port, err => {
   if (err) throw err;
